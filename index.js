43191e48a883cc13d239512d32150dc6ac93a46c
Egonboz <egonboz@gmail.com>
2025-02-18 18:14:28 +0100

    🐛 login reload error fixed

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const path = require('path');
 const { dbConnection } = require('./database/config');
 require('dotenv').config();
 const cors = require('cors');
@@ -15,6 +16,10 @@ app.use(express.json());
 
 app.use("/api/auth", require('./routes/auth'));
 app.use("/api/events", require('./routes/events'));
+
+app.use("*", (req, res) => {
+    res.sendFile(path.join( __dirname + '/public/index.html' ));
+});
 
 app.listen(process.env.PORT, () => {
     console.log(`Server is running on port ${process.env.PORT}`);
